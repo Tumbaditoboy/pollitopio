c6eb4f2f524083f37750f2faf386d4a4a3f541fb
dzlan <dzlan@10.232.10.217>
2024-05-13 12:46:28 -0600

    Creación del proyecto

---
+++ src/main/java/mx/itson/pollitopio/pollitopio/PollitoPio.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.pollitopio.pollitopio;
+
+/**
+ *
+ * @author dzlan
+ */
+public class PollitoPio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
